1d5525f6bc47d2a57060e83a99b8dacafc29b79b
Kamil_K <kkubuszek@zs1.nowotarski.edu.pl>
2021-02-22 21:22:52 +0100

    Remove hello world app code

--- src/main/kotlin/service/App.kt
+++ src/main/kotlin/service/App.kt
@@ -3,13 +3,6 @@
  */
 package service
 
-class App {
-    val greeting: String
-        get() {
-            return "Hello world."
-        }
-}
-
 fun main(args: Array<String>) {
-    println(App().greeting)
+
 }
