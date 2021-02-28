977692190a07180f5c522bc8488b9fea413cdc0a
Pawel <jankowskipawelxd@gmail.com>
2021-02-28 14:38:44 +0100

    Fix package paths

--- src/main/kotlin/service/models/List.kt
+++ src/main/kotlin/service/models/List.kt
@@ -1,3 +1,3 @@
-package service
+package service.models
 
 data class List(val id: String, var name: String, val author: String, var tasks: Array<Task>)
--- src/main/kotlin/service/models/Task.kt
+++ src/main/kotlin/service/models/Task.kt
@@ -1,4 +1,4 @@
-package service
+package service.models
 
 import java.time.ZonedDateTime
 import java.util.*
