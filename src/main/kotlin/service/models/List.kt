cc190c1382503ede2c7e1ab7f7ecf276423595fd
Pawel <jankowskipawelxd@gmail.com>
2021-02-28 17:47:59 +0100

    Add models

--- src/main/kotlin/service/models/List.kt
+++ src/main/kotlin/service/models/List.kt
@@ -1,3 +1,16 @@
 package service.models
 
-data class List(val id: String, var name: String, val author: String, var tasks: Array<Task>)
+import org.jetbrains.exposed.dao.*
+
+//data class List(val id: String, var name: String, val author: String, var tasks: Array<Task>)
+
+object Lists : IntIdTable() {
+    val name = varchar("name", 100)
+    val author = varchar("author", 100)
+}
+
+class List(id: EntityID<Int>) : IntEntity(id) {
+    companion object : IntEntityClass<List>(Lists)
+    var name by Lists.name
+    var author by Lists.author
+}
--- src/main/kotlin/service/models/Task.kt
+++ src/main/kotlin/service/models/Task.kt
@@ -1,6 +1,24 @@
 package service.models
 
-import java.time.ZonedDateTime
-import java.util.*
+import org.jetbrains.exposed.dao.*
 
-data class Task(var title: String, val id: String, var dueDate: ZonedDateTime, var priority: Int, var isDone: Boolean, var description: String)
+//data class Task(var title: String, val id: String, var dueDate: ZonedDateTime, var priority: Int, var isDone: Boolean, var description: String)
+
+object Tasks : IntIdTable() {
+    val title = varchar("title", 100)
+    val dueTime = datetime("dueTime")
+    val priority = integer("priority")
+    val isDone = bool("isDone")
+    val description = varchar("description", 256)
+    val list = reference("list", Lists)
+}
+
+class Task(id: EntityID<Int>) : IntEntity(id) {
+    companion object : IntEntityClass<Task>(Tasks)
+
+    var title by Tasks.title
+    var dueTime by Tasks.dueTime
+    var priority by Tasks.priority
+    var isDone by Tasks.isDone
+    var description by Tasks.description
+}
